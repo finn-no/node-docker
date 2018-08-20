538de9cf9047bbc2a2c947e2fb4f561f2b32bbbd
Martin Lehmann <martin.lehmann@finn.no>
2018-08-20 16:46:38 +0200

    Make SECRETS_DIR an env var defaulting to /var/run/secrets/fiaas

--- scripts/run-app.sh
+++ scripts/run-app.sh
@@ -9,7 +9,7 @@ log() {
 export_secrets_from_dir() {
   local secrets_dir=$1
 
-  for filename in $secrets_dir/*; do
+  for filename in $SECRETS_DIR/*; do
     local filename_without_path=${filename##*/}
     local filename_uppercased=$(echo $filename_without_path | tr 'a-z' 'A-Z');
     local filename_with_underscores=${filename_uppercased//-/_}
@@ -37,20 +37,19 @@ startup() {
   if [ -z ${FIAAS_ENVIRONMENT} ]; then
     log "FIAAS_ENVIRONMENT is unset, not looking for secrets";
   else
-    local secrets_dir="/var/run/secrets/fiaas"
-    if [ ! -d "$secrets_dir" ]; then
-      log "Secrets directory '$secrets_dir' does not exist, not looking for secrets";
+    if [ ! -d "$SECRETS_DIR" ]; then
+      log "Secrets directory '$SECRETS_DIR' does not exist, not looking for secrets";
     else
-      log "FIAAS_ENVIRONMENT is set to '$FIAAS_ENVIRONMENT', looking for secrets in '$secrets_dir'";
+      log "FIAAS_ENVIRONMENT is set to '$FIAAS_ENVIRONMENT', looking for secrets in '$SECRETS_DIR'";
 
       # Count number of files, then trim leading whitespace
-      local secret_count=$(find $secrets_dir -type f | wc -l | tr -d ' ')
+      local secret_count=$(find $SECRETS_DIR -type f | wc -l | tr -d ' ')
 
       if [ $secret_count -eq 0 ]; then
-        log "Found no secrets in '$secrets_dir'"
+        log "Found no secrets in '$SECRETS_DIR'"
       else
-        log "Found '$secret_count' secret(s) in '$secrets_dir'"
-        export_secrets_from_dir $secrets_dir
+        log "Found '$secret_count' secret(s) in '$SECRETS_DIR'"
+        export_secrets_from_dir $SECRETS_DIR
       fi
     fi
   fi
