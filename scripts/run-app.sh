e05a4dadd23f7d9008c0202a9845990e2abb364c
Martin Lehmann <martin.lehmann@finn.no>
2018-08-20 10:37:07 +0200

    Export secrets if present

--- scripts/run-app.sh
+++ scripts/run-app.sh
@@ -1,3 +1,33 @@
 #!/usr/bin/env sh
 
-echo 'hi'
+export_secrets_from_dir() {
+  local secrets_dir=$1
+
+  for filename in $secrets_dir/*; do
+    local filename_without_path=${filename##*/}
+    local filename_uppercased=$(echo $filename_without_path | tr 'a-z' 'A-Z');
+    local filename_with_underscores=${filename_uppercased//-/_}
+
+    local secret_name="SECRET_$filename_with_underscores";
+
+    echo "Exporting secret $filename_without_path as $secret_name"
+
+    local secret=$(<$filename);
+
+    export "$secret_name=$secret"
+  done
+}
+
+if [ -z ${FIAAS_ENVIRONMENT} ]; then
+  echo "FIAAS_ENVIRONMENT is unset, not looking for secrets";
+else
+  local secrets_dir="/var/run/secrets/fiaas"
+  if [ ! -d "$secrets_dir" ]; then
+    echo "Secrets directory '$secrets_dir' does not exist, not looking for secrets";
+  else
+    echo "FIAAS_ENVIRONMENT is set to '$FIAAS_ENVIRONMENT', looking for secrets in '$secrets_dir'";
+    export_secrets_from_dir $secrets_dir
+  fi
+fi
+
+node .
