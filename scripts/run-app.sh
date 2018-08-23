e218df885f44d4bd7f7f56c182e74ec45f6014c3
Martin Lehmann <martin.lehmann@finn.no>
2018-08-23 14:50:51 +0200

    Fix shellcheck violations

--- scripts/run-app.sh
+++ scripts/run-app.sh
@@ -1,24 +1,22 @@
 #!/usr/bin/env sh
 
 log() {
-  local message=$1
-  local iso_8601_timestamp=$(date +%Y-%m-%dT%H:%M:%S%z)
-  echo '{"@version": 1, "@timestamp": "'"$iso_8601_timestamp"'", "level": "INFO", "message": "'"$1"'"}'
+  message=$1
+  iso_8601_timestamp=$(date +%Y-%m-%dT%H:%M:%S%z)
+  echo '{"@version": 1, "@timestamp": "'"$iso_8601_timestamp"'", "level": "INFO", "message": "'"$message"'"}'
 }
 
 export_secrets_from_dir() {
-  local secrets_dir=$1
+  for filename in "$SECRETS_DIR"/*; do
+    filename_without_path=${filename##*/}
+    filename_uppercased=$(echo "$filename_without_path" | tr '[:lower:]' '[:upper:]');
+    filename_with_underscores="$(echo "$filename_uppercased" | tr '-' '_')"
 
-  for filename in $SECRETS_DIR/*; do
-    local filename_without_path=${filename##*/}
-    local filename_uppercased=$(echo $filename_without_path | tr 'a-z' 'A-Z');
-    local filename_with_underscores=${filename_uppercased//-/_}
-
-    local secret_name="SECRET_$filename_with_underscores";
+    secret_name="SECRET_$filename_with_underscores";
 
     log "Exporting secret '$filename_without_path' as '$secret_name'"
 
-    local secret=$(cat $filename);
+    secret=$(cat "$filename");
 
     export "$secret_name=$secret"
   done
@@ -26,30 +24,33 @@ export_secrets_from_dir() {
 
 start_app() {
   # npm provides binaries on PATH in scripts, so we have to do the same
-  export PATH=$PATH:$(pwd)/node_modules/.bin
+  path_with_node_modules_binaries=$PATH:$(pwd)/node_modules/.bin
+  export PATH=$path_with_node_modules_binaries
 
-  local start_script=$(node -p "require('./package.json').scripts.start")
+  # Fetch 'start' script from package.json
+  start_script=$(node -p "require('./package.json').scripts.start")
 
+  # Execute the script
   $start_script
 }
 
 startup() {
-  if [ -z ${FIAAS_ENVIRONMENT} ]; then
-    log "FIAAS_ENVIRONMENT is unset, not looking for secrets";
+  if [ -z "$FIAAS_ENVIRONMENT" ]; then
+    log "FIAAS_ENVIRONMENT is unset; not looking for secrets";
   else
     if [ ! -d "$SECRETS_DIR" ]; then
-      log "Secrets directory '$SECRETS_DIR' does not exist, not looking for secrets";
+      log "Secrets directory '$SECRETS_DIR' does not exist; not looking for secrets";
     else
-      log "FIAAS_ENVIRONMENT is set to '$FIAAS_ENVIRONMENT', looking for secrets in '$SECRETS_DIR'";
+      log "FIAAS_ENVIRONMENT is set to '$FIAAS_ENVIRONMENT'; looking for secrets in '$SECRETS_DIR'";
 
       # Count number of files, then trim leading whitespace
-      local secret_count=$(find $SECRETS_DIR -type f | wc -l | tr -d ' ')
+      secret_count=$(find "$SECRETS_DIR" -type f | wc -l | tr -d ' ')
 
-      if [ $secret_count -eq 0 ]; then
+      if [ "$secret_count" -eq 0 ]; then
         log "Found no secrets in '$SECRETS_DIR'"
       else
         log "Found $secret_count secret(s) in '$SECRETS_DIR'"
-        export_secrets_from_dir $SECRETS_DIR
+        export_secrets_from_dir
       fi
     fi
   fi
