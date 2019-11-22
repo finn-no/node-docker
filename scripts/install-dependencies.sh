7e94d1847a2c8e8644918cf16a9d708c8192845c
henarons <henrik.brautaset.aronsen@finn.no>
2019-11-22 09:45:30 +0100

    Fail build if npm/yarn wants to update their lock files
    Use `npm ci` and `yarn install --frozen-lockfile` to fail build
    if yarn.lock / package-lock.json / npm-shrinkwrap.json isn't updated
    before pushing.
    
    This makes sure that the dependencies in the built image aren't
    different from the ones used locally.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-if [ -f "/home/node/src/yarn.lock" ];
-  then
-    yarn install --pure-lockfile
-    # Check if the installed tree is correct. Install all dependencies if not
-    yarn check --verify-tree || NODE_ENV=development yarn install
-    yarn cache clean
-  else
-    npm install
-    npm cache clean --force
+if [ -f "/home/node/src/yarn.lock" ]; then
+  yarn install --frozen-lockfile
+  # Check if the installed tree is correct. Install all dependencies if not
+  yarn check --verify-tree || NODE_ENV=development yarn install
+  yarn cache clean
+elif [ -f "/home/node/src/package-lock.json" ] || [ -f "/home/node/src/npm-shrinkwrap.json" ]; then
+  npm ci
+  npm cache clean --force
+else
+  npm install
+  npm cache clean --force
 fi
