749ac3b13bbd12737b63f4fa3633441f5cb55cc3
henarons <henrik.brautaset.aronsen@finn.no>
2019-11-25 09:02:41 +0100

    Only verify lockfile if FAIL_ON_DIRTY_LOCKFILE is set

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,13 +2,21 @@
 
 set -e
 
+if [ -n "${FAIL_ON_DIRTY_LOCKFILE}" ]; then
+  YARN_OPTS="--frozen-lockfile"
+  NPM_CMD="ci"
+else
+  YARN_OPTS=""
+  NPM_CMD="install"
+fi
+
 if [ -f "/home/node/src/yarn.lock" ]; then
-  yarn install --frozen-lockfile
+  yarn install $YARN_OPTS
   # Check if the installed tree is correct. Install all dependencies if not
   yarn check --verify-tree || NODE_ENV=development yarn install
   yarn cache clean
 elif [ -f "/home/node/src/package-lock.json" ] || [ -f "/home/node/src/npm-shrinkwrap.json" ]; then
-  npm ci
+  npm $NPM_CMD
   npm cache clean --force
 else
   npm install
